f5a72fa3959882b16c7cba9d488ea4b827a5d8f4
samhi113 <samhiggins113@gmail.com>
2021-09-15 12:23:29 -0400

    Finished L3 programs

--- L3-Return Methods/src/TrianglePerimeter.java
+++ L3-Return Methods/src/TrianglePerimeter.java
@@ -14,27 +14,38 @@ public class TrianglePerimeter {
      */
 
     public static void main(String[] args) {
-        DecimalFormat roundTo2 = new DecimalFormat("#,###.00");
+        int x1 = getInput("What is the x coordinate for the first point?");
+        int y1 = getInput("What is the y coordinate for the first point?");
+        int x2 = getInput("What is the x coordinate for the second point?");
+        int y2 = getInput("What is the y coordinate for the second point?");
+        int x3 = getInput("What is the x coordinate for the third point?");
+        int y3 = getInput("What is the y coordinate for the third point?");
 
-        int x1 = Integer.parseInt(JOptionPane.showInputDialog("What is the x coordinate for the first point?"));
-        int y1 = Integer.parseInt(JOptionPane.showInputDialog("What is the y coordinate for the first point?"));
-        int x2 = Integer.parseInt(JOptionPane.showInputDialog("What is the x coordinate for the second point?"));
-        int y2 = Integer.parseInt(JOptionPane.showInputDialog("What is the y coordinate for the second point?"));
-        int x3 = Integer.parseInt(JOptionPane.showInputDialog("What is the x coordinate for the third point?"));
-        int y3 = Integer.parseInt(JOptionPane.showInputDialog("What is the y coordinate for the third point?"));
+        double side1 = calcDistance(x1, y1, x2, y2);
+        double side2 = calcDistance(x3, y3, x2, y2);
+        double side3 = calcDistance(x1, y1, x3, y3);
 
-        double perimeter = Double.parseDouble(roundTo2.format(calcPer(x1, y1, x2, y2, x3, y3)));
+        double perimeter = calcPer(side1, side2, side3);
 
-        JOptionPane.showMessageDialog(null, "The perimeter of the triangle is " + perimeter);
+        outputResults(perimeter);
 
-        System.exit((int)(perimeter*100));
+        System.exit(69);
+    }
+    public static int getInput(String message) {
+        return Integer.parseInt(JOptionPane.showInputDialog(message));
     }
 
-    public static double calcPer(int x1, int y1, int x2, int y2, int x3, int y3) {
-        double side1 = Math.sqrt(Math.pow(Math.abs(x1-x2),2) + Math.pow(Math.abs(y1-y2),2));
-        double side2 = Math.sqrt(Math.pow(Math.abs(x3-x2),2) + Math.pow(Math.abs(y3-y2),2));
-        double side3 = Math.sqrt(Math.pow(Math.abs(x1-x3),2) + Math.pow(Math.abs(y1-y3),2));
+    public static double calcDistance(int x1, int y1, int x2, int y2){
+        return Math.sqrt(Math.pow(Math.abs(x1-x2),2) + Math.pow(Math.abs(y1-y2),2));
+    }
 
+    public static double calcPer(double side1, double side2, double side3) {
         return side1 + side2 + side3;
+    }
+
+    public static void outputResults(double perimeter) {
+        DecimalFormat roundTo2 = new DecimalFormat("#,###.00");
+
+        JOptionPane.showMessageDialog(null, "The perimeter of the triangle is " + roundTo2.format(perimeter));
     }
 }
