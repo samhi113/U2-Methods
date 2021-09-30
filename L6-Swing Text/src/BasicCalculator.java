3cd1e656b675a6de11baf9deef9a6c292a671335
samhi113 <samhiggins113@gmail.com>
2021-09-30 11:42:35 -0400

    Finished L6 programs

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
 public class BasicCalculator {
 
     /* Create a calculator program with two text fields
@@ -8,4 +13,83 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
+
+    static JTextField input1 = new JTextField(5);
+    static JTextField input2 = new JTextField(5);
+    static JLabel outputField = new JLabel("Your answer: ");
+    static DecimalFormat roundTo4 = new DecimalFormat("#.####");
+
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Sam's Calculator");
+        JPanel panel = new JPanel();
+        JButton addButton = new JButton("+");
+        JButton subButton = new JButton("-");
+        JButton mulButton = new JButton("x");
+        JButton divButton = new JButton("/");
+
+        window.setSize(500, 100);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        addButton.addActionListener(new addListener());
+        subButton.addActionListener(new subListener());
+        mulButton.addActionListener(new mulListener());
+        divButton.addActionListener(new divListener());
+
+        panel.add(input1);
+        panel.add(addButton);
+        panel.add(subButton);
+        panel.add(mulButton);
+        panel.add(divButton);
+        panel.add(input2);
+        panel.add(outputField);
+
+        window.add(panel);
+        window.setVisible(true);
+    }
+
+    private static class addListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(input1.getText());
+            double num2 = Double.parseDouble(input2.getText());
+
+            double sum = num1 + num2;
+
+            outputField.setText("Your answer: " + roundTo4.format(sum));
+        }
+    }
+
+    private static class subListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(input1.getText());
+            double num2 = Double.parseDouble(input2.getText());
+
+            double difference = num1 - num2;
+
+            outputField.setText("Your answer: " + roundTo4.format(difference));
+        }
+    }
+
+    private static class mulListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(input1.getText());
+            double num2 = Double.parseDouble(input2.getText());
+
+            double product = num1 * num2;
+
+            outputField.setText("Your answer: " + roundTo4.format(product));
+        }
+    }
+
+    private static class divListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(input1.getText());
+            double num2 = Double.parseDouble(input2.getText());
+
+            double quotient = num1 / num2;
+
+            outputField.setText("Your answer: " + roundTo4.format(quotient));
+        }
+    }
 }
--- L6-Swing Text/src/SimpleInput.java
+++ L6-Swing Text/src/SimpleInput.java
@@ -1,4 +1,41 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SimpleInput {
 
+    static int clickCount = 0;
+    static JLabel clicksLabel = new JLabel("Clicker Game by Sam!");
+    static JTextField textInput = new JTextField(11);
 
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Clicker Game");
+        JPanel panel = new JPanel();
+        JLabel titleLabel = new JLabel("Product name:");
+        JButton submitButton = new JButton("Submit");
+
+        window.setSize(300,400);
+        window.setLocationRelativeTo(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        submitButton.addActionListener(new ButtonListener());
+
+        panel.add(titleLabel);
+        panel.add(textInput);
+        panel.add(clicksLabel);
+        panel.add(submitButton);
+
+        window.add(panel);
+        window.setVisible(true);
+    }
+
+    private static class ButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            String product = textInput.getText();
+
+            clickCount++;
+
+            clicksLabel.setText("Sold " + clickCount + " number of " + product);
+        }
+    }
 }
